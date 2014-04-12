f56e1de70a3f47e72e37332ad8ad156ae8bbeef4
Ji-Yuhang <yuhang.silence@gmail.com>
2014-04-12 10:17:03 +0800

    修复插入不进去的BUG

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ void show_help()
     printf("Type \"lname\", Show Data order by Name\n");
     printf("Type \"lprice\", Show Data order by Price\n");
     printf("\n");
-
+    
 }
 
 int write_to_file(struct information* info)
@@ -60,7 +60,7 @@ int write_to_file(struct information* info)
     fprintf(file, "%s\n", info->company);
     fprintf(file, "%s\n", info->comment);
     fputs("\n",file);
-
+    
     fclose(file);
     return 1;
 }
@@ -71,7 +71,7 @@ struct info_list* read_from_file(struct info_list* list_head)
     struct information *info = NULL;
     struct info_list *list_next = NULL;
     struct info_list *list_temp = NULL;
-
+    
     char str[400];
     int flag = 0;
     
@@ -88,9 +88,11 @@ struct info_list* read_from_file(struct info_list* list_head)
             ++flag;
         else
             continue;
-
-        if (flag > 7)
-            flag = 0;
+        
+        if (flag > 7) {
+            flag = 1;
+            
+        }
         
         switch(flag)
         {
@@ -105,9 +107,9 @@ struct info_list* read_from_file(struct info_list* list_head)
                 }
                 list_next->next = NULL;
                 list_next->node = NULL;
-
+                
                 list_temp->node = info;
-                list_temp->next = list_next; 
+                list_temp->next = list_next;
                 list_temp = list_next;
                 info->id = atol(str);
                 break;
@@ -137,10 +139,10 @@ struct info_list* read_from_file(struct info_list* list_head)
                 break;
         }
     }
-
+    
     fclose(file);
     return list_head;
-
+    
 }
 
 void insert_information(struct info_list *list_head)
@@ -151,7 +153,7 @@ void insert_information(struct info_list *list_head)
     struct information *info = NULL;
     struct info_list* list_next = NULL;
     struct info_list* list_temp = NULL;
-
+    
     if (!list_head) {
         list_head = (struct info_list*)malloc(sizeof(struct info_list));
         if (NULL == list_head) {
@@ -164,7 +166,7 @@ void insert_information(struct info_list *list_head)
     list_temp = list_head;
     while (list_temp != NULL && list_temp->node != NULL)
         list_temp = list_temp->next;
-
+    
     list_next = (struct info_list*)malloc(sizeof(struct info_list));
     info = (struct information*)malloc(sizeof(struct information));
     if (NULL == info || NULL == list_next) {
@@ -172,16 +174,17 @@ void insert_information(struct info_list *list_head)
         return ;
     }
     list_next->next = NULL;
-    list_next->node = info;
+    list_next->node = NULL;
     list_temp->next = list_next;
+    list_temp->node = info;
     printf("\nNow Insert a New Data of Devcie.\n");
-
+    
     printf("Plase Enter ID, only numeral support, for example: \"123456\"\n");
     printf(">>>>");
     fgets(id_char, 200, stdin);
     info->id = atol(id_char);
     printf("You Typed %ld\n", info->id);
-
+    
     printf("Plase Enter Name, for example: \"Kobe Bryant\"\n");
     printf(">>>>");
     fgets(info->name, 200, stdin);
@@ -193,26 +196,26 @@ void insert_information(struct info_list *list_head)
     fgets(info->type, 200, stdin);
     trimmed(info->type);
     printf("You Typed %s\n", info->type);
-
-
+    
+    
     printf("Plase Enter Price, only numeral support, example: \"123.456\"\n");
     printf(">>>>");
     fgets(price_char, 200, stdin);
     info->price = atof(price_char);
     printf("You Typed %f\n", info->price);
-
+    
     printf("Plase Enter Number, only numeral support, example: \"543210\"\n");
     printf(">>>>");
     fgets(number_char, 200, stdin);
     info->number = atoi(number_char);
     printf("You Typed %u\n", info->number);
-
+    
     printf("Plase Enter Company, for example: \"Red Had\"\n");
     printf(">>>>");
     fgets(info->company, 200, stdin);
     trimmed(info->company);
     printf("You Typed %s\n", info->company);
-
+    
     printf("Plase Enter Comment, for example: \"This is Comment\"\n");
     printf(">>>>");
     fgets(info->comment, 400, stdin);
@@ -254,28 +257,28 @@ void select_operator(char operator[100], struct info_list * list_head)
     int compare_delete = -1;
     int compare_search = -1;
     int compare_modify = -1;
-
-/*1.*/
+    
+    /*1.*/
     compare_help = strcmp(operator,"help\n");
     if (0 == compare_help)
         show_help();
-/*2.*/
+    /*2.*/
     compare_insert = strcmp(operator, "v\n");
     if (0 == compare_insert)
         insert_information(list_head);
-/*3.*/
+    /*3.*/
     compare_list = strcmp(operator, "l\n");
     if (0 == compare_list)
         list_information(list_head);
-/*4.*/
+    /*4.*/
     compare_delete = strcmp(operator, "d\n");
     if (0 == compare_delete)
         delete_information();
-/*5.*/
+    /*5.*/
     compare_search = strcmp(operator, "s\n");
     if (0 == compare_search)
         search_information();
-/*6.*/
+    /*6.*/
     compare_modify = strcmp(operator, "m\n");
     if (0 == compare_modify)
         modify_information();
@@ -286,7 +289,7 @@ int main(int argc, char** argv)
     struct info_list * list_head = (struct info_list*)malloc(sizeof(struct info_list));
     if (NULL == list_head) {
         printf("\nError!    Cannot malloc memory\n");
-        return ;
+        return 0;
     }
     list_head->next = NULL;
     list_head->node = NULL;;
@@ -301,6 +304,7 @@ int main(int argc, char** argv)
         select_operator(operator, list_head);
         
     } while (strcmp(operator, "q\n") != 0);
-
+    
     return 0;
 }
+
